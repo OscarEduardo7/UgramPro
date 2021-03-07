42ddd35a2492751d1bc29037681551e8a8784113
Oscar Mazariegos <oscarmazariegoss5@gmail.com>
2021-03-07 02:27:18 -0600

    backend2 endpoints

--- Backend2/main.py
+++ Backend2/main.py
@@ -58,6 +58,218 @@ def login2():
              
     return item
 
+##editar Usuarios
+@app.route('/editarUsuarios', methods=['POST'])
+def editarUsuarios():
+    if request.method == 'POST':
+        data = request.get_json(force=False)
+        
+        usuario = data.get('userName')
+        nombre = data.get('nombre')
+        apellido = data.get('apellido')
+        foto = data.get('foto')
+        
+        table = dynamodb.Table('Usuarios')
+        
+        response = table.update_item(
+            Key={
+                "userName": usuario
+            },
+            UpdateExpression="set nombre= :nom, apellido= :las, foto= :miF",
+            ExpressionAttributeValues={
+            ":nom": nombre,
+            ":las": apellido,
+            ":miF": foto
+            },
+            ReturnValues= "UPDATED_NEW"
+        )            
+    return response
+
+
+##editar Usuarios
+@app.route('/getAlbumes', methods=['POST'])
+def getAlbumes():
+    if request.method == 'POST':
+        data = request.get_json(force=False)
+        
+        usuario = data.get('userName')
+        
+        table = dynamodb.Table('Albumes')
+        
+        response = table.scan(
+            FilterExpression = 'idUser = :n',
+            ExpressionAttributeValues = {':n' : usuario}
+        )
+        items = response['Items']
+        
+    return response
+
+## insertar nuevo album
+@app.route('/newAlbum', methods=['POST'])
+def newAlbum():
+    if request.method == 'POST':
+        data = request.get_json(force=False)
+        
+        usuario = data.get('userName')
+        titulo = data.get('titulo')
+        
+        table = dynamodb.Table('Albumes')
+        
+        r = table.put_item(
+            Item={
+                "id": usuario+"_"+titulo,
+                "idUser": usuario,
+                "titulo": titulo
+            }
+        )
+        msg = "Se creo el album"       
+    return "Creado"
+
+## Eliminar album
+@app.route('/deleteAlbum', methods=['POST'])
+def deleteAlbum():
+    if request.method == 'POST':
+        data = request.get_json(force=False)
+        
+        usuario = data.get('userName')
+        titulo = data.get('titulo')
+        
+        table = dynamodb.Table('Albumes')
+        
+        try:
+            
+            response = table.delete_item(
+                Key={
+                    "id": usuario+"_"+titulo
+                }
+            )
+        except ClientError as e:
+            if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+                print(e.response['Error']['Message'])
+            else:
+                raise
+        else:
+            return "{}"
+
+#-- Registrar nuevo usuario ------------
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        data = request.get_json(force=False)
+        
+        usuario = data.get('userName')
+        nombre = data.get('nombre')
+        apellido = data.get('apellido')
+        contra = data.get('contra')
+        foto = data.get('foto')
+        
+        table = dynamodb.Table('Usuarios')
+        
+        r = table.put_item(
+            Item={
+                "userName": usuario,
+                "nombre": nombre,
+                "apellido": apellido,
+                "contra": contra,
+                "foto": foto
+            }
+        )       
+    return "suceess"
+
+# -- Todos2 ---- obtiene todos los usuarios
+@app.route('/todos2', methods=['GET'])
+def todos2():
+    if request.method == 'GET':
+       
+        table = dynamodb.Table('Usuarios')
+        
+        response = table.scan(
+            Limit= 100
+        )        
+    return response
+
+
+#-- guardarFotoPerfil ------------
+@app.route('/guardarFotoPerfil', methods=['POST'])
+def guardarFotoP():
+    if request.method == 'POST':
+        data = request.get_json(force=False)
+        
+        idFoto = data.get('idFoto')
+        idUser = data.get('idUser')
+        ubicacion = data.get('ubicacion')
+        
+        table = dynamodb.Table('FotosPerfil')
+        
+        r = table.put_item(
+            Item={
+                "idFoto": idFoto,
+                "idUser": idUser,
+                "ubicacion": ubicacion,
+            }
+        )       
+    return "suceess"
+
+#-- guardarFotoPerfil ------------
+@app.route('/guardarFotos', methods=['POST'])
+def guardarFotos():
+    if request.method == 'POST':
+        data = request.get_json(force=False)
+        
+        idFoto = data.get('idFoto')
+        usuario = data.get('userName')
+        album = data.get('album')
+        nombre = data.get('nombreI')
+        ubicacion = data.get('ubicacion')
+        
+        table = dynamodb.Table('FotosPublicadas')
+        
+        r = table.put_item(
+            Item={
+                "idFoto": idFoto,
+                "album": album,
+                "nombreFoto": nombre,
+                "ubicacion": ubicacion,
+                "usuario": usuario
+            }
+        )       
+    return "suceess"
+
+##---- Mostrar Fotos ------
+@app.route('/fotosPerfilUsuario', methods=['POST'])
+def fotosPerfilUsuario():
+    if request.method == 'POST':
+        data = request.get_json(force=False)
+        
+        usuario = data.get('userName')
+        
+        table = dynamodb.Table('FotosPerfil')
+        
+        response = table.scan(
+            FilterExpression = 'idUser = :n',
+            ExpressionAttributeValues = {':n' : usuario}
+        )
+        items = response['Items']
+        
+    return response
+
+##---- Fotos subidas por el usuario ------
+@app.route('/fotosPublicadasUsuario', methods=['POST'])
+def fotosPublicadasUsuario():
+    if request.method == 'POST':
+        data = request.get_json(force=False)
+        
+        usuario = data.get('userName')
+        
+        table = dynamodb.Table('FotosPublicadas')
+        
+        response = table.scan(
+            FilterExpression = 'usuario = :n',
+            ExpressionAttributeValues = {':n' : usuario}
+        )
+        items = response['Items']
+        
+    return response
 
 
 # se puede setear el puerto que queremos utilizar
